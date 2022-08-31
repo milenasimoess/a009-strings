4f60c70c8e639f8aea021f2ae632ad9823a27971
milenarismo <milenasimoeslabenu@gmail.com>
2022-08-31 15:24:57 -0300

    Exercícios finalizados 009

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual seu nome?")
+const comida1 = prompt("Qual sua comida favorita?")
+const comida2 = prompt("Qual sua segunda comida favorita?")
+const comida3 = prompt("Qaul sua terceira comida favorita?")
+
+console.log(`Estas são as comidas favoritas de ${nome} : \n${comida1} \n${comida2} \n${comida3} .`)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,29 @@
 //Crie a const para a frase aqui
+
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres:"BOAS VINDAS, mas não deixe o gato sair".`
+console.log(frase)
+
+const novafrase = frase.replaceAll("verde" , "rosa") 
+const novafrase2 = novafrase.replaceAll("azul" , "laranja")
+console.log(novafrase2)
+
+console.log(novafrase2.includes("verde") && novafrase2.includes("laranja"))
+
+let fraseExtra = novafrase2.replace("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR")
+console.log(fraseExtra)
+
+
+
+// const frase3 = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS,`
+// const extra = `mas não deixe o gato sair.`
+// const fraseMaiuscula = extra.toUpperCase()
+// console.log(frase3 + fraseMaiuscula)
+
+//outra maneira de fazer:
+//const novafrase = frase.replace("verde","rosa").replace("azul","laranja")
+
+
+
+
+
+
